f502e3950dfe25885a4e6bd38fc65ca45b3bfe67
Sujan <sujanlamichhane742@example.com>
2021-09-29 02:22:18 +0545

    Page Transition done

--- lib/screens/main/components/bottom_navigation_bar/bottom_navbar_views/bottom_appbar_container.dart
+++ lib/screens/main/components/bottom_navigation_bar/bottom_navbar_views/bottom_appbar_container.dart
@@ -1,44 +1,105 @@
+
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:sabalive/screens/main/components/bottom_navigation_bar/bottom_navbar_views/bottom_navbar_item.dart';
+import 'package:sabalive/app_properties.dart';
 import 'package:sabalive/screens/main/components/bottom_navigation_bar/bottom_navigationbar_controller/bottom_navbar_controller.dart';
+import 'package:sabalive/screens/shop/cart_page_view.dart';
 
-class BottomAppBarContainer extends StatelessWidget {
-final BottomNavBarController _bottomNavBarController= Get.put(BottomNavBarController());
-final TabController controller;
-
-  BottomAppBarContainer({Key key,
-    @required this.controller}) : super(key: key);
+class BottomNavBar extends StatelessWidget {
+  final BottomNavBarController bottomNavBarController = Get.put(BottomNavBarController());
+  
   @override
   Widget build(BuildContext context) {
-    
-    return BottomAppBar(
-      child: Container(
-        height: 60,
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-          children: [
-            MyBottomNavigationBarItem(
-                controller: _bottomNavBarController,
-                tabController: controller,
-                currentScreenIndex: 0,
-                icon: Icons.home,
-                label: "Home"),
-            MyBottomNavigationBarItem(
-              tabController: controller,
-                controller: _bottomNavBarController,
-                currentScreenIndex: 1,
-                icon: Icons.shopping_cart,
-                label: "Cart"),
-            MyBottomNavigationBarItem(
-              tabController: controller,
-                controller: _bottomNavBarController,
-                currentScreenIndex: 2,
-                icon: Icons.person,
-                label: "Profile"),
-          ],
-        ),
+    final Size size = MediaQuery.of(context).size;
+    return Container(
+      width: size.width,
+      height: 65,
+      child: Stack(
+        children: [
+          CustomPaint(
+            size: Size(size.width, 80),
+            painter: BNBCustomPainter(),
+          ),
+          Center(
+            heightFactor: 0.9,
+            child: FloatingActionButton(
+                backgroundColor: darkgreen,
+                child: Icon(Icons.shopping_basket),
+                elevation: 0.1,
+                onPressed: () {
+                  Get.to(() => CartPageView());
+                }),
+          ),
+          Obx(
+                () => Container(
+              width: size.width,
+              height: 80,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  IconButton(
+                    splashRadius: 5,
+                    icon: Icon(
+                      Icons.home,
+                      color: bottomNavBarController.currentIndex.value == 0
+                          ? darkgreen
+                          : Colors.grey.shade400,
+                    ),
+                    onPressed: () {
+                      bottomNavBarController.setBottomBarIndex(
+                          0,  Icon(Icons.search_outlined));
+                      
+                      print(bottomNavBarController.currentIndex.value);
+                    },
+                    splashColor: Colors.white,
+                  ),
+                  IconButton(
+                      splashRadius: 3,
+                      icon: Icon(
+                        Icons.favorite,
+                        color: bottomNavBarController.currentIndex.value == 1
+                            ? darkgreen
+                            : Colors.grey.shade400,
+                      ),
+                      onPressed: () {
+                        bottomNavBarController.setBottomBarIndex(
+                            1,  Icon(Icons.search_outlined));
+                      }),
+                
+                ],
+              ),
+            ),
+          )
+        ],
       ),
     );
+  }
+}
+
+class BNBCustomPainter extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {
+    Paint paint = new Paint()
+      ..color = Colors.white
+      ..style = PaintingStyle.fill;
+    
+    Path path = Path();
+    path.moveTo(0, 0); // Start
+    path.quadraticBezierTo(size.width * 0.20, 0, size.width * 0.35, 0);
+    path.quadraticBezierTo(size.width * 0.40, 0, size.width * 0.40, 30);
+    path.arcToPoint(Offset(size.width * 0.60, 20),
+        radius: Radius.circular(20.0), clockwise: false);
+    path.quadraticBezierTo(size.width * 0.60, 0, size.width * 0.65, 0);
+    path.quadraticBezierTo(size.width * 0.80, 0, size.width, 0);
+    path.lineTo(size.width, size.height);
+    path.lineTo(0, size.height);
+    path.lineTo(0, 0);
+    canvas.drawShadow(path, Colors.black, 5, true);
+    canvas.drawPath(path, paint);
+  }
+  
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) {
+    return false;
   }
 }
--- lib/screens/main/main_page.dart
+++ lib/screens/main/main_page.dart
@@ -1,9 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_swiper/flutter_swiper.dart';
+import 'package:get/get.dart';
 import 'package:sabalive/screens/main/components/bottom_navigation_bar/bottom_navbar_views/bottom_appbar_container.dart';
 import 'package:sabalive/screens/main/components/homepage/homepage.dart';
 import 'package:sabalive/screens/main/widgets/drawer.dart';
 import 'package:sabalive/screens/profile_page.dart';
 import 'package:sabalive/screens/shop/cart_page_view.dart';
+import '../../app_properties.dart';
+import 'components/bottom_navigation_bar/bottom_navigationbar_controller/bottom_navbar_controller.dart';
 
 class MainPage extends StatefulWidget {
   @override
@@ -12,33 +16,55 @@ class MainPage extends StatefulWidget {
 
 class _MainPageState extends State<MainPage>
     with TickerProviderStateMixin<MainPage> {
+  final BottomNavBarController bottomNavBarController =
+  Get.put(BottomNavBarController());
+  
+  SwiperController swiperController;
+  TabController tabController;
   TabController bottomTabController;
-
+  
   @override
   void initState() {
     super.initState();
+    tabController = TabController(length: 5, vsync: this);
     bottomTabController = TabController(length: 3, vsync: this);
   }
-
+  
   @override
   Widget build(BuildContext context) {
-    List<Widget> _screens = [
-      HomePage(
+    Widget tabBar = TabBar(
+      tabs: [
+        Tab(text: 'In Demand'),
+        Tab(text: 'Red meat'),
+        Tab(text: 'White meat'),
+        Tab(text: 'Fish'),
+        Tab(text: 'Sea food'),
+      ],
+      labelStyle: TextStyle(fontSize: 16.0),
+      unselectedLabelStyle: TextStyle(
+        fontSize: 14.0,
       ),
-      // CategoryListPage(),
-      CartPageView(),
-      ProfilePage(),
-    ];
-
-    return Scaffold(
-      drawer: drawer(context),
-      bottomNavigationBar: BottomAppBarContainer(
-        controller: bottomTabController,
-      ),
-      body: TabBarView(
-          controller: bottomTabController,
-          physics: NeverScrollableScrollPhysics(),
-          children: _screens),
+      labelColor: darkGrey,
+      unselectedLabelColor: Color.fromRGBO(0, 0, 0, 0.5),
+      isScrollable: true,
+      controller: tabController,
     );
+    
+    return Scaffold(
+        drawer: drawer(context),
+        bottomNavigationBar: BottomNavBar(),
+        body: GetBuilder<BottomNavBarController>(
+            init: BottomNavBarController(),
+            builder: (data) {
+              switch (data.currentIndex.value) {
+                case 1:
+                  return ProfilePage();
+                default:
+                  return HomePage(
+                    tabBar: tabBar,
+                    tabController: bottomTabController,
+                  );
+              }
+            }));
   }
 }
